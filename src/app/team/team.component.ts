cb5548a9f29c9b80bae3116c2f1ed5f448771799
Sed9yDataBank <benzidsedki@gmail.com>
2020-03-18 22:05:01 +0100

    Join Page Done

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,7 +13,7 @@ export class TeamComponent implements OnInit {
 
   images = ["joinslider1.jpg", "joinslider2.jpg"].map((n) => `assets/img/${n}`);
 
-  constructor(config: NgbCarouselConfig) { 
+  constructor(config: NgbCarouselConfig, private router: Router) { 
     config.interval = 6000;
     config.wrap = true;
     config.keyboard = false;
@@ -22,4 +23,10 @@ export class TeamComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToBreweries() {
+    this.router.onSameUrlNavigation = "reload";
+    this.router.navigate(["/joinus"], { fragment: "breweries" }).finally(() => {
+        this.router.onSameUrlNavigation = "ignore"; // Restore config after navigation completes
+    });
+  }
 }
