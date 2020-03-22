dc934fcc4d750ddd8782a0ed213b7afee655a2fb
Sed9yDataBank <benzidsedki@gmail.com>
2020-03-22 13:52:21 +0100

    Admin Reply

--- src/app/mailbox/mailbox.component.ts
+++ src/app/mailbox/mailbox.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MailService } from './mailservice.service';
 import { Mailbox } from './mailbox';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,11 +12,19 @@ import { Mailbox } from './mailbox';
 export class MailboxComponent implements OnInit {
 
   mailBox : Mailbox [];
-  
-  constructor(private service: MailService) { }
+  showModal: boolean;
+  replyForm: FormGroup;
+  sent: boolean = false;
+
+  constructor(private service: MailService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.getAllMessages();
+    this.replyForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],        
+      subject: ['', [Validators.required, Validators.minLength(6)]],
+      message: ['', [Validators.required, Validators.minLength(5)]],
+    });
   }
 
   getAllMessages() {
@@ -36,4 +45,31 @@ export class MailboxComponent implements OnInit {
       );
 	}
   
+  show()
+  {
+    this.showModal = true; // Show-Hide Modal Check
+    
+  }
+  //Bootstrap Modal Close event
+  hide()
+  {
+    this.showModal = false;
+  }
+  clearForm(){
+    (<HTMLFormElement>document.getElementById("connection")).reset();
+  }
+  get f() { return this.replyForm.controls; }
+  onSent() {
+    this.sent = true;
+    // stop here if form is invalid
+    if (this.replyForm.invalid) {
+        return;
+    }
+    if(this.sent)
+    {
+      this.showModal = false;
+      this.replyForm.reset();
+    }
+   
+}
 }
